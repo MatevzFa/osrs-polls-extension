7c2839ba1847e62e616d6b4afb1b07ad5df5b68c
Matevž Fabjančič <mf6422@student.uni-lj.si>
2017-11-16 11:48:40 +0100

    Fixed content_scripts.matches URL in manifest.json

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
+$(document).ready(() => {
 
-$('fieldset.question').each((i) => {
+    $('fieldset.question').each((i) => {
 
-    let $curQuestion = $('fieldset.question').eq(i);
+        let $curQuestion = $('fieldset.question').eq(i);
 
-    let $answers = $curQuestion.find('table');
+        let $answers = $curQuestion.find('table');
 
-    let $ansYes = $answers.find('tr').eq(0);
-    let $ansNo = $answers.find('tr').eq(1);
-    let $ansSkip = $answers.find('tr').eq(2);
+        let $ansYes = $answers.find('tr').eq(0);
+        let $ansNo = $answers.find('tr').eq(1);
+        let $ansSkip = $answers.find('tr').eq(2);
 
-    if ($ansYes.find('td').eq(0).text() == 'Yes' && $ansNo.find('td').eq(0).text() == 'No') {
+        if ($ansYes.find('td').eq(0).text() == 'Yes' && $ansNo.find('td').eq(0).text() == 'No') {
 
-        let yes = parseInt((/\((\d*) votes\)/g).exec($ansYes.find('td').eq(2).text())[1]);
-        let no = parseInt((/\((\d*) votes\)/g).exec($ansNo.find('td').eq(2).text())[1]);
-        let skip = $ansSkip.find('td').eq(2).text();
-        if (skip)
-            skip = parseInt((/\((\d*) votes\)/g).exec(skip)[1]);
-        let validVotes = yes + no;
+            let yes = parseInt((/\((\d*) votes\)/g).exec($ansYes.find('td').eq(2).text())[1]);
+            let no = parseInt((/\((\d*) votes\)/g).exec($ansNo.find('td').eq(2).text())[1]);
+            let skip = $ansSkip.find('td').eq(2).text();
+            if (skip)
+                skip = parseInt((/\((\d*) votes\)/g).exec(skip)[1]);
+            let validVotes = yes + no;
 
-        if (yes / validVotes >= 0.75) {
-            $curQuestion.addClass('passed');
-        } else {
-            $curQuestion.append($('<p />', {
-                css: {
-                    'text-align': 'center',
-                    'margin': '3px 0px'
-                },
-                text: 'This question needs ' + votesTillPassing(yes, validVotes) + ' more votes to pass.'
-            }));
-            $curQuestion.addClass('failed');
+            if (yes / validVotes >= 0.75) {
+                $curQuestion.addClass('passed');
+            } else {
+                $curQuestion.append($('<p />', {
+                    css: {
+                        'text-align': 'center',
+                        'margin': '3px 0px'
+                    },
+                    text: 'This question needs ' + votesTillPassing(yes, validVotes) + ' more votes to pass.'
+                }));
+                $curQuestion.addClass('failed');
+            }
+
+            $ansYes.find('td').eq(2).text(resultString(yes, validVotes));
+            $ansNo.find('td').eq(2).text(resultString(no, validVotes));
+            if (skip)
+                $ansSkip.find('td').eq(2).text(skip + ' votes');
+
+            $ansYes.find('td').eq(1).find('img').eq(1).attr('width', swordWidth(yes, validVotes) + 'px');
+            $ansNo.find('td').eq(1).find('img').eq(1).attr('width', swordWidth(no, validVotes) + 'px');
+
+            if (skip)
+                $ansSkip.find('td').eq(1).find('img').css('opacity', 0);
         }
-
-        $ansYes.find('td').eq(2).text(resultString(yes, validVotes));
-        $ansNo.find('td').eq(2).text(resultString(no, validVotes));
-        if (skip)
-            $ansSkip.find('td').eq(2).text(skip + ' votes');
-
-        $ansYes.find('td').eq(1).find('img').eq(1).attr('width', swordWidth(yes, validVotes) + 'px');
-        $ansNo.find('td').eq(1).find('img').eq(1).attr('width', swordWidth(no, validVotes) + 'px');
-
-        if (skip)
-            $ansSkip.find('td').eq(1).find('img').css('opacity', 0);
-    }
+    });
 });
 
 function resultString(votes, total) {
